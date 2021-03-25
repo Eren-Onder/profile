bfdacd6cc5caf41ce8e96747bf89160293e700c6
E2395-Eren <2395-Eren@users.noreply.github.com>
2021-03-25 23:23:32 +0100

    profil

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+
+
+class Banner extends Component {
+    render() {
+        return (
+            <section className="container-banner">
+
+                    <a href="#home"></a>
+                    <h1> Hi! I'm Eren ÖNDER </h1>
+                   
+                    
+                    
+            </section>
+        )
+    }
+}
+
+export default Banner
